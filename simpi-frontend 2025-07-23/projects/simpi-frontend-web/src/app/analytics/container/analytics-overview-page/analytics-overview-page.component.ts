4902a06050ddd9ebe77b9188e15aac83cb02b12a
Jochen <jochen.hildebrandt@avano.com>
2025-07-25 01:27:38 +0200

    add loading spinner to analytics page

--- simpi-frontend 2025-07-23/projects/simpi-frontend-web/src/app/analytics/container/analytics-overview-page/analytics-overview-page.component.ts
+++ simpi-frontend 2025-07-23/projects/simpi-frontend-web/src/app/analytics/container/analytics-overview-page/analytics-overview-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Observable } from "rxjs";
+import { finalize } from "rxjs/operators";
 import { AnalyticsService } from "projects/simpi-frontend-common/src/lib/services/analytics/analytics.service";
 import { UserInteractionStatsResponse } from "projects/simpi-frontend-common/src/lib/models/http/responses/userInteractionStatsResponse";
 import { DropdownMenuHelperService } from "../../../shared/services/dropdown-menu-helper.service";
@@ -10,28 +11,57 @@ import { getStartOfLastDay } from "../../date-calculation";
 @Component({
   selector: "sim-analytics-overview-page",
   template: `
-    <sim-analytics-overview
-      [userInteractionStats]="userInteractionStats$ | async"
-    >
-    </sim-analytics-overview>
-    <sim-analytics-table
-      [userInteractionStats]="userInteractionStats$ | async"
-      (startDateChanged)="startDateChangedCall($event)"
-      (cloneSimpi)="cloneSimpi($event)"
-      (showProductSettings)="showProductSettings($event)"
-      (shareProduct)="shareProduct()"
-      (shareSimpi)="shareSimpi($event)"
-      (editSimpiSteps)="navigateToSimpiStepEditor($event)"
-      (changeSimpiSettings)="changeSimpiSettings($event)"
-      (deleteSimpi)="deleteSimpi($event)"
-    ></sim-analytics-table>
+    <div class="analytics-container">
+      <div class="loading-overlay" *ngIf="isLoading">
+        <sim-loading-spinner [show]="true"></sim-loading-spinner>
+      </div>
+      <ng-container *ngIf="userInteractionStats$ | async as stats; else loading">
+        <sim-analytics-overview
+          [userInteractionStats]="stats"
+        >
+        </sim-analytics-overview>
+        <sim-analytics-table
+          [userInteractionStats]="stats"
+          (startDateChanged)="startDateChangedCall($event)"
+          (cloneSimpi)="cloneSimpi($event)"
+          (showProductSettings)="showProductSettings($event)"
+          (shareProduct)="shareProduct()"
+          (shareSimpi)="shareSimpi($event)"
+          (editSimpiSteps)="navigateToSimpiStepEditor($event)"
+          (changeSimpiSettings)="changeSimpiSettings($event)"
+          (deleteSimpi)="deleteSimpi($event)"
+        ></sim-analytics-table>
+      </ng-container>
+      <ng-template #loading>
+        <sim-loading-spinner [show]="true"></sim-loading-spinner>
+      </ng-template>
+    </div>
   `,
+  styles: [`
+    .analytics-container {
+      position: relative;
+      min-height: 400px;
+    }
+    .loading-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: rgba(255, 255, 255, 0.8);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 1000;
+    }
+  `]
 })
 export class AnalyticsOverviewPageComponent implements OnInit {
 
   public userInteractionStats$: Observable<UserInteractionStatsResponse>;
   public userInteractionStats: UserInteractionStatsResponse;
   public errorLoadingData: boolean = false;
+  public isLoading: boolean = false;
 
   constructor(
     private analyticsService: AnalyticsService,
@@ -41,14 +71,19 @@ export class AnalyticsOverviewPageComponent implements OnInit {
   ) {}
 
   public ngOnInit(): void {
-    this.userInteractionStats$ = this.analyticsService.getUserInteractionStats(
-      getStartOfLastDay()
-    );
+    this.loadAnalyticsData(getStartOfLastDay());
+  }
+
+  private loadAnalyticsData(date: Date): void {
+    this.isLoading = true;
+    this.userInteractionStats$ = this.analyticsService.getUserInteractionStats(date)
+      .pipe(
+        finalize(() => this.isLoading = false)
+      );
   }
 
   public startDateChangedCall(date: Date) {
-    this.userInteractionStats$ =
-      this.analyticsService.getUserInteractionStats(date);
+    this.loadAnalyticsData(date);
   }
 
   public navigateToProductSettings(productAlias: string) {
