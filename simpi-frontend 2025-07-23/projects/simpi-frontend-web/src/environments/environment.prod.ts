5386173a999848cb1c2040c33f0669a710600d57
Jochen <jochen.hildebrandt@avano.com>
2025-08-01 01:19:10 +0200

    changed baseURL in QR code modal

--- simpi-frontend 2025-07-23/projects/simpi-frontend-web/src/environments/environment.prod.ts
+++ simpi-frontend 2025-07-23/projects/simpi-frontend-web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://simpi.com/app',
+  baseUrl: 'https://app.simpi.ai',
   restUrl: 'https://simpi.com/simpis',
   userServiceUrl: 'https://simpi.com/users',
   mobileCreatedSimpisProductId: '35895f4b-57fb-4404-b81c-70a90076a431',
